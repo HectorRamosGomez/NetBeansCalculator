e4d97528eec9b44e428f97e6deef5ded82201e01
Hector Ramos Gomez <hector.s453554@cesurformacion.com>
2025-10-14 13:29:22 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/calculator/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+import com.mycompany.calculator.calculatorview.MainView;
+
+/**
+ *
+ * @author HectorRamosGomez
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MainView mv = new MainView();
+        mv.setVisible(true);
+    }
+}
